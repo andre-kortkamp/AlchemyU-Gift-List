50a78cf21b648dba7528863adc032a80a74ffc27
andre.kortkamp <andre.kortkamp@genezys.io>
2023-05-23 15:59:47 -0300

    solution gift list project

--- client/index.js
+++ client/index.js
@@ -7,8 +7,20 @@ const serverUrl = 'http://localhost:1225';
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  // get the name inserted by the user
+  const name = "Wanda Ratke";
+
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+
+  // find the proof that the name is in the list 
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof,
+    name
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const Blob = require('node:buffer').Blob;
 
 const port = 1225;
 
@@ -8,14 +9,19 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'c3f7f8bef6769c83cf77f88176da0690b09a47b2bec7c70912687201492ae7dd';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { proof, name } = req.body;
+
+  // find byte size of Merkle root
+  const myBlob = new Blob([MERKLE_ROOT]);
+  const byteSize = myBlob.size;
+  console.log(`Size of ${MERKLE_ROOT} is ${byteSize} bytes`);
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
